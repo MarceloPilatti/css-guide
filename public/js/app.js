b5db526f82ae3605aa8f8325c50df5f096339457
Marcelo Pilatti <marcelo.silva979@gmail.com>
2019-04-30 16:40:27 -0300

    .

--- public/js/app.js
+++ public/js/app.js
@@ -94,24 +94,8 @@ const setEvents = () => {
         
             target.setAttribute('data-x', x);
             target.setAttribute('data-y', y);
-          }
-      })
-    //   .on('resizemove', function (event) {
-    //     var target = event.target,
-    //         x = (parseFloat(target.getAttribute('data-x')) || 0),
-    //         y = (parseFloat(target.getAttribute('data-y')) || 0);
-    
-    //     target.style.width  = event.rect.width + 'px';
-    //     target.style.height = event.rect.height + 'px';
-    
-    //     x += event.deltaRect.left;
-    //     y += event.deltaRect.top;
-    
-    //     target.style.webkitTransform = target.style.transform ='translate(' + x + 'px,' + y + 'px)';
-    
-    //     target.setAttribute('data-x', x);
-    //     target.setAttribute('data-y', y);
-    //   });;
+        }
+    });
 }
 
 (()=>{
